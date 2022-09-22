dda9cf23600fa3298aa5a8aee2bded09b9b94f83
Igor Nascimento Costa <69372874+IgorNCosta14@users.noreply.github.com>
2022-09-22 15:17:04 -0300

    Added header

--- loja-demo/src/routes/componentes/header/header.tsx
+++ loja-demo/src/routes/componentes/header/header.tsx
@@ -1,11 +1,11 @@
 export function Header() {
-    return(
-        <header className="bg-blue-300">
+    return (
+        <header className="fixed top-0 right-0 left-0 z-999 bg-white relative-ns">
             <div data-collapse="medium" data-animation="default" data-duration="400" data-easing="ease-out"
-                className="bg-blue-300">
-                <nav role="navigation" className="nav-menu w-nav-menu"><a href="/" className="bg-blue-300">
-                    <div className="">Ini­cio</div>
-                    <div className=""></div>
+                className="wrap cc-menu w-nav cby-container"><a href="/" className="imageTeste brand w-nav-brand w--current"></a>
+                <nav role="navigation" className="nav-menu w-nav-menu"><a href="/" className="menu-item-wrap w--current">
+                    <div className="menu-item">Ini­cio</div>
+                    <div className="menu-item-deco-line"></div>
                 </a><a href="/vestidos" className="menu-item-wrap">
                     <div className="menu-item">Moda</div>
                     <div className="menu-item-deco-line"></div>
@@ -13,8 +13,16 @@ export function Header() {
                     <div className="menu-item">Casa</div>
                     <div className="menu-item-deco-line"></div>
                 </a>
+                {/* <!--<a href="/oficina" className="menu-item-wrap">-->
+                <!--    <div className="menu-item">Oficina</div>-->
+                <!--    <div className="menu-item-deco-line"></div>-->
+                <!--</a>-->
+                <!--<a href="/books" className="menu-item-wrap">-->
+                <!--    <div className="menu-item">Books</div>-->
+                <!--    <div className="menu-item-deco-line"></div>-->
+                <!--</a>--> */}
                 </nav>
-                <div className="relative dib"><p className="bg-red-600">oi</p><a href="javascript:cartData.open()" data-node-type="commerce-cart-open-link"
+                <div className="relative dib"><a href="javascript:cartData.open()" data-node-type="commerce-cart-open-link"
                     className="cart-button"><img
                     src="data:image/svg+xml;charset=UTF-8,%3csvg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' version='1.1' id='Capa_1' x='0px' y='0px' viewBox='0 0 489 489' style='enable-background:new 0 0 489 489;' xml:space='preserve'%3e%3cg%3e%3cpath d='M440.1,422.7l-28-315.3c-0.6-7-6.5-12.3-13.4-12.3h-57.6C340.3,42.5,297.3,0,244.5,0s-95.8,42.5-96.6,95.1H90.3 c-7,0-12.8,5.3-13.4,12.3l-28,315.3c0,0.4-0.1,0.8-0.1,1.2c0,35.9,32.9,65.1,73.4,65.1h244.6c40.5,0,73.4-29.2,73.4-65.1 C440.2,423.5,440.2,423.1,440.1,422.7z M244.5,27c37.9,0,68.8,30.4,69.6,68.1H174.9C175.7,57.4,206.6,27,244.5,27z M366.8,462 H122.2c-25.4,0-46-16.8-46.4-37.5l26.8-302.3h45.2v41c0,7.5,6,13.5,13.5,13.5s13.5-6,13.5-13.5v-41h139.3v41 c0,7.5,6,13.5,13.5,13.5s13.5-6,13.5-13.5v-41h45.2l26.9,302.3C412.8,445.2,392.1,462,366.8,462z'/%3e%3c/g%3e%3c/svg%3e"
                     width="16" alt="" className="cart-icon"/>
@@ -37,13 +45,14 @@ export function Header() {
                         </div>
                         </div>
                     </div>
-                    {/* <script>
-                        $('.portal-totalizers-ref').minicart({
+                    <script>
+                        {/* $('.portal-totalizers-ref').minicart({
                         showMinicart: false,
                         showTotalizers: true,
                         showShippingOptions: false
-                        });
-                    </script> */}
+                        }); */}
+                    </script>
+
                     </div>
                     <div className="menu-item-deco-line"></div>
                 </a></div>
