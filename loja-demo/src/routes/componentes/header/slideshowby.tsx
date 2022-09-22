71a5eb4c4676901ed0abc7674352ddf65df84226
Igor Nascimento Costa <69372874+IgorNCosta14@users.noreply.github.com>
2022-09-22 18:52:16 -0300

    updated

---
+++ loja-demo/src/routes/componentes/header/slideshowby.tsx
@@ -0,0 +1,8 @@
+export function SlideShowBy() {
+    return (
+        <div >
+            <img className="imageRose" src="https://imgur.com/N6Eu3AF.png" />
+        </div>
+        
+    )
+}
--- loja-demo/src/routes/index.server.tsx
+++ loja-demo/src/routes/index.server.tsx
@@ -1,7 +1,13 @@
 import { Header } from "./componentes/header/header";
+import { SlideShowBy } from "./componentes/header/slideshowby";
 
 export default function Home() {
   return (
+    <>
       <Header />
+      <SlideShowBy />
+
+      <p className="w-full h-600px">produtos aqui</p>
+    </>
   )
 }
